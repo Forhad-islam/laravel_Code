6b17dcf5b779a264a7ef6e7f783a9723ca4a8aca
MD. FORHADUL ISLAM <103302239+Forhad-islam@users.noreply.github.com>
2023-09-23 15:36:43 +0600

    Add files via upload

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,1 @@
+<h1> Forhad demo  </h1>
---
+++ resources/views/name.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data From Controller</title>
+</head>
+<body>
+    <h1> Hello, {{ $name }}</h1>
+    <h1> Email: {{ $email }}</h1>
+    
+</body>
+</html>
